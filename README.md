5ebac972ee6cea023cdd15772fd6981fc9225ac1
Sazid <mahmuduzzamansazid@gmail.com>
2025-05-04 14:50:29 +0600

    Iitial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# iamsazid
+
+A new Flutter project.
+
+<p>Click here to visit website</p> <a href="">Click</a>
---
+++ android/app/src/main/kotlin/com/sazid/iamsazid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sazid.iamsazid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
